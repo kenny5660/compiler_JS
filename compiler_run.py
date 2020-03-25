171c09ebf08c236520a4e1b4ade5883a4040c77c
kenny5660 <32442117+kenny5660@users.noreply.github.com>
2020-03-25 18:47:34 +1000

    add print parse tree

--- compiler_run.py
+++ compiler_run.py
@@ -1,6 +1,7 @@
 import sys
 
 import antlr4 
+from antlr4.tree.Trees import Trees
 from antlr4_js_parser.ECMAScriptLexer import ECMAScriptLexer
 from antlr4_js_parser.ECMAScriptParser import ECMAScriptParser
 from antlr4_js_parser.ECMAScriptListener import ECMAScriptListener
@@ -8,10 +9,13 @@ from antlr4_js_parser.ECMAScriptListener import ECMAScriptListener
 def main(argv):
     input_stream = antlr4.FileStream(argv[1])
     lexer = ECMAScriptLexer(input_stream)
+    token = lexer.nextToken()
+    token2 = lexer.nextToken()
     stream = antlr4.CommonTokenStream(lexer)
     parser = ECMAScriptParser(stream)
     tree = parser.program()
-    print(tree)
+    print(Trees.toStringTree(tree, None, parser))
+   # print(tree.toStringTree())
  
 if __name__ == '__main__':
     #main()
