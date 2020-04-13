d85ddd745d14bbebed27a8383e86dff97bf81d97
kenny5660 <32442117+kenny5660@users.noreply.github.com>
2020-04-13 17:25:23 +1000

    change function name

--- compiler_js/console_utility.py
+++ compiler_js/console_utility.py
@@ -4,10 +4,7 @@ def main():
     if len(sys.argv) < 2:
         print("Error: no input files")
         return -1
-
-    print("Hello")
-    print(sys.argv)
-    compiler_js.core.compile_file(sys.argv[1])
+    compiler_js.core.parse_file(sys.argv[1])
  
 if __name__ == '__main__':
     #main()
--- compiler_js/core.py
+++ compiler_js/core.py
@@ -6,16 +6,16 @@ from compiler_js.antlr4_ecmas.ECMAScriptLexer import ECMAScriptLexer
 from compiler_js.antlr4_ecmas.ECMAScriptParser import ECMAScriptParser
 from compiler_js.antlr4_ecmas.ECMAScriptListener import ECMAScriptListener
 
-def compile_file(path):
+def parse_file(path):
     input_stream = antlr4.FileStream(path)
-    compile_js_stream(input_stream)
+    parse_js_stream(input_stream)
 
-def compile_js_stream(input_stream):
+def parse_js_stream(input_stream):
     lexer = ECMAScriptLexer(input_stream)
     token = lexer.nextToken()
     token2 = lexer.nextToken()
     stream = antlr4.CommonTokenStream(lexer)
     parser = ECMAScriptParser(stream)
     tree = parser.program()
-    print(Trees.toStringTree(tree, None, parser))
+    return tree
    # print(tree.toStringTree())
